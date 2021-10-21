a2a748620111f6dad6622a278e5149e5d2d24d2b
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2021-10-21 13:55:01 -0400

    Oct-21-2021_1:55_PM

--- 04_functions/main.go
+++ 04_functions/main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+func greeting(name string) string {
+	return "Hello " + name
+}
+
+func getSum(param1 int, param2 int) int {
+	return param1 + param2
+}
+
 func main(){
-	fmt.Println("Hello world.")
+	fmt.Println(greeting("Bill"))
+	fmt.Println(getSum(5, 25))
 }
---
+++ 05_arrays_slices/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main(){
+	// Arrays
+	var fruitArr [3]string
+
+	fruitArr[0]="Apple"
+	fruitArr[1]="Orange"
+	fruitArr[2]="Banana"
+
+	fmt.Println(fruitArr)
+
+}

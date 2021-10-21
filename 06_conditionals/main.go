c6e04d25dbe6fae213c17aae1d73113a93cc6078
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2021-10-21 14:15:43 -0400

    Oct-21-2021_2:15_PM

--- 06_conditionals/main.go
+++ 06_conditionals/main.go
@@ -3,5 +3,33 @@ package main
 import "fmt"
 
 func main(){
-	fmt.Println("Hello world.")
+	x := 5
+	y := 10
+
+	if x < y {
+		fmt.Printf("If: %d is less than %d\n", x, y)
+	} else {
+		fmt.Printf("Else: %d is less than %d\n", y, x)
+	}
+
+	color := "red"
+
+	if color == "red" {
+		fmt.Printf("color is red\n")
+	} else if color == "blue" {
+		fmt.Printf("color is blue\n")
+	} else {
+		fmt.Printf("color is something else\n")
+	}
+
+	//switch
+	switch color {
+	case "red":
+		fmt.Printf("color is red\n")
+	case "blue":
+		fmt.Printf("color is blue\n")
+	default:
+		fmt.Printf("color is something else\n")
+	}
+
 }

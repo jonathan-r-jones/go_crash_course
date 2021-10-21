7d87dc8461534e8ac869d047d9a323a2a0703efd
Jonathan R. Jones <jonathan.r.jones.3@gmail.com>
2021-10-21 14:50:23 -0400

    Oct-21-2021_2:50_PM

--- 08_maps/main.go
+++ 08_maps/main.go
@@ -7,7 +7,17 @@ func main(){
 	emails:=make(map[string]string)
 	emails["Bob"] = "bob@gmail.com"
 	emails["Sharon"] = "sharon@gmail.com"
+	emails["Mike"] = "mike@gmail.com"
 
 	fmt.Println(emails)
 	fmt.Println(emails["Bob"])
+	fmt.Println(len(emails))
+
+	// Delete Bob's ass.
+	delete(emails, "Bob")
+	fmt.Println(emails)
+
+	emails2:=map[string]string{"tim": "tim@gmail.com", "Jerry": "Jerry@gmail.com"}
+	fmt.Println(emails2)
+
 }
---
+++ 09_range/main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main(){
+	ids := []int{33, 76, 54, 23, 11, 2}
+	for i, id:=range ids {
+		fmt.Printf("%d - ID: %d\n", i, id)
+	}
+	for _, id:=range ids {
+		fmt.Printf("ID: %d\n", id)
+	}
+}
